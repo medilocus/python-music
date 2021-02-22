609186ded043d5cfbf873be7c220d162223e1cd1
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-21 22:51:21 -0800

    Changed argument name. Prevents conflicts with builtins.

--- music/note.py
+++ music/note.py
@@ -24,40 +24,40 @@ class Note:
     """Class for a single note."""
 
     pitch: int
-    type: int
+    note_type: int
     dots: int
 
-    def __init__(self, pitch: int, type: int, dots: int = 0) -> None:
+    def __init__(self, pitch: int, note_type: int, dots: int = 0) -> None:
         """
         Initializes note.
         :param pitch: Integer pitch of note. The lowest note on piano is 21.
-        :param type: Note length, defined in constants: music.NOTE_QUARTER, music.NOTE_HALF, ...
+        :param note_type: Note length, defined in constants: music.NOTE_QUARTER, music.NOTE_HALF, ...
         """
         self.pitch = pitch
-        self.type = type
+        self.note_type = note_type
         self.dots = dots
 
     @staticmethod
-    def type_to_len(type: int) -> float:
+    def type_to_len(note_type: int) -> float:
         """
         Converts from a type (music.NOTE_QUARTER) to a length (1)
-        :param type: Type of note.
+        :param note_type: Type of note.
         """
-        if type == NOTE_QUARTER:
+        if note_type == NOTE_QUARTER:
             return 1
-        elif type == NOTE_HALF:
+        elif note_type == NOTE_HALF:
             return 2
-        elif type == NOTE_WHOLE:
+        elif note_type == NOTE_WHOLE:
             return 4
-        elif type == NOTE_DOUBLE_WHOLE:
+        elif note_type == NOTE_DOUBLE_WHOLE:
             return 8
-        elif type == NOTE_8TH:
+        elif note_type == NOTE_8TH:
             return 1 / 2
-        elif type == NOTE_16TH:
+        elif note_type == NOTE_16TH:
             return 1 / 4
-        elif type == NOTE_32TH:
+        elif note_type == NOTE_32TH:
             return 1 / 8
-        elif type == NOTE_64TH:
+        elif note_type == NOTE_64TH:
             return 1 / 16
 
-        raise ValueError(f"Note type {type} not implemented.")
+        raise ValueError(f"Note type {note_type} not implemented.")
